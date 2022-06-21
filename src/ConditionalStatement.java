de8adca6e3b66d8fb6037c4d687088dab9a55249
Justin Adkins <justinadkins25@yahoo.com>
2022-06-21 08:22:53 -0700

    commited

---
+++ src/ConditionalStatement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+
+
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        int z;
+
+        System.out.println("Enter a number: ");
+        x = scan.nextInt();
+        System.out.println("Enter a second number: ");
+        y = scan.nextInt();
+        System.out.println("Enter a third number: ");
+        z = scan.nextInt();
+
+        if (x > y && y > z) {
+            System.out.println("DECREASING");
+        }
+        else if ( z > y && y > x){
+            System.out.println("INCREASING");
+        } else{
+            System.out.println("NEITHER");
+        }
+
+
+
+
+
+    }
+
+}
